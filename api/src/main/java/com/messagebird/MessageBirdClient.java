b541b3a94d0963e6ef5a96e43be62ffff9794107
Marius Potgieter <mariuspotgieter@gmail.com>
2019-09-06 15:51:09 +0200

    Force CI rebuild phase 2

--- api/src/main/java/com/messagebird/MessageBirdClient.java
+++ api/src/main/java/com/messagebird/MessageBirdClient.java
@@ -369,7 +369,6 @@ public class MessageBirdClient {
         return messageBirdService.sendPayLoad(VERIFYPATH, verifyRequest, Verify.class);
     }
 
-    
     /**
      * @param recipient The telephone number that you want to verify.
      * @return Verify object
