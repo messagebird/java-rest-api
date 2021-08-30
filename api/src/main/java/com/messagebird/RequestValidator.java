4ce603c4821e89aaaccbc855ba7ed1ba9d5778b8
khanh.nguyen <khanh.nguyen@messagebird.com>
2021-08-30 12:10:30 +0200

    Updated webhook signature jwt implementation and tests

--- api/src/main/java/com/messagebird/RequestValidator.java
+++ api/src/main/java/com/messagebird/RequestValidator.java
@@ -1,8 +1,5 @@
 package com.messagebird;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier.BaseVerification;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -13,29 +10,60 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 import com.messagebird.exceptions.RequestValidationException;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * RequestValidator validates webhook signature signed by MessageBird services.
+ *
+ * @see <a href="https://developers.messagebird.com/docs/verify-http-requests">Verify HTTP Requests</a>
  */
 public class RequestValidator {
 
+    /**
+     * Signature of signed request is set with header name 'MessageBird-Signature-JWT'
+     */
     public static final String SIGNATURE_HEADER = "MessageBird-Signature-JWT";
-
     private static final String ALGORITHM_SHA256 = "SHA-256";
+    private static final char[] HEX_DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd',
+            'e', 'f'};
 
-    private static final char[] HEX_DIGITS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd',
-            'e', 'f' };
-
-    private final String signatureKey;
+    private final Algorithm HMAC256, HMAC384, HMAC512;
 
     /**
      * RequestValidator validates webhook signature with a customer signature key.
-     * 
-     * @param signatureKey customer signature key
+     *
+     * @param signatureKey customer signature key. Can be retrieved through <a href="https://dashboard.messagebird.com/developers/settings">Developer Settings</a>. This is NOT your API key.
+     * @see <a href="https://developers.messagebird.com/docs/verify-http-requests">Verify HTTP Requests</a>
      */
     public RequestValidator(String signatureKey) {
-        this.signatureKey = signatureKey;
+        this.HMAC256 = Algorithm.HMAC256(signatureKey);
+        this.HMAC384 = Algorithm.HMAC384(signatureKey);
+        this.HMAC512 = Algorithm.HMAC512(signatureKey);
     }
 
+    /**
+     * Returns raw signature payload after validating a signature successfully,
+     * otherwise throws {@code RequestValidationException}.
+     * <p>
+     * This JWT is signed with a MessageBird account unique secret key, ensuring the request is from MessageBird and a specific account.
+     * The JWT contains the following claims:</p>
+     * <ul>
+     *   <li>"url_hash" - the raw URL hashed with SHA256 ensuring the URL wasn't altered (validated by default)</li>
+     *   <li> "payload_hash" - the raw payload hashed with SHA256 ensuring the payload wasn't altered (validated by default)</li>
+     *   <li> "jti" - a unique token ID to implement an optional non-replay check (NOT validated by default)</li>
+     *   <li> "nbf" - the not before timestamp (validated by default)</li>
+     *   <li> "exp" - the expiration timestamp is ensuring that a request isn't captured and used at a later time. (validated by default)</li>
+     *   <li> "iss" - the issuer name, always MessageBird (validated by default)</li>
+     * </ul>
+     *
+     * @param clock       custom {@link Clock} instance to validate timestamp claims.
+     * @param signature   the actual signature.
+     * @param url         the raw url including the protocol, hostname and query string, https://example.com/?example=42.
+     * @param requestBody the raw request body.
+     * @return raw signature payload as {@link DecodedJWT} object.
+     * @throws RequestValidationException when the signature is invalid.
+     */
     public DecodedJWT validateSignature(Clock clock, String signature, String url, byte[] requestBody)
             throws RequestValidationException {
         DecodedJWT jwt = JWT.decode(signature);
@@ -43,16 +71,16 @@ public class RequestValidator {
         Algorithm algorithm;
         switch (jwt.getAlgorithm()) {
             case "HS256":
-                algorithm = Algorithm.HMAC256(this.signatureKey);
+                algorithm = HMAC256;
                 break;
             case "HS384":
-                algorithm = Algorithm.HMAC384(this.signatureKey);
+                algorithm = HMAC384;
                 break;
             case "HS512":
-                algorithm = Algorithm.HMAC512(this.signatureKey);
+                algorithm = HMAC512;
                 break;
             default:
-                throw new RequestValidationException("The signing method is invalid.");
+                throw new RequestValidationException(String.format("The signing method '%s' is invalid.", jwt.getAlgorithm()));
         }
 
         BaseVerification builder = (BaseVerification) JWT.require(algorithm)
@@ -88,6 +116,27 @@ public class RequestValidator {
         }
     }
 
+    /**
+     * Returns raw signature payload after validating a signature successfully,
+     * otherwise throws {@code RequestValidationException}.
+     * <p>
+     * This JWT is signed with a MessageBird account unique secret key, ensuring the request is from MessageBird and a specific account.
+     * The JWT contains the following claims:</p>
+     * <ul>
+     *   <li>"url_hash" - the raw URL hashed with SHA256 ensuring the URL wasn't altered (validated by default)</li>
+     *   <li> "payload_hash" - the raw payload hashed with SHA256 ensuring the payload wasn't altered (validated by default)</li>
+     *   <li> "jti" - a unique token ID to implement an optional non-replay check (NOT validated by default)</li>
+     *   <li> "nbf" - the not before timestamp (validated by default)</li>
+     *   <li> "exp" - the expiration timestamp is ensuring that a request isn't captured and used at a later time. (validated by default)</li>
+     *   <li> "iss" - the issuer name, always MessageBird (validated by default)</li>
+     * </ul>
+     *
+     * @param signature   the actual signature.
+     * @param url         the raw url including the protocol, hostname and query string, https://example.com/?example=42.
+     * @param requestBody the raw request body.
+     * @return raw signature payload as {@link DecodedJWT} object.
+     * @throws RequestValidationException when the signature is invalid.
+     */
     public DecodedJWT validateSignature(String signature, String url, byte[] requestBody)
             throws RequestValidationException {
         return validateSignature(null, signature, url, requestBody);
--- api/src/test/java/com/messagebird/RequestValidatorTest.java
+++ api/src/test/java/com/messagebird/RequestValidatorTest.java
@@ -1,54 +1,28 @@
 package com.messagebird;
 
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
-
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.time.OffsetDateTime;
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import javax.annotation.Resources;
-
 import com.auth0.jwt.interfaces.Clock;
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.messagebird.exceptions.RequestValidationException;
-
+import com.messagebird.util.Resources;
 import org.junit.Test;
 import org.junit.function.ThrowingRunnable;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.OffsetDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
 @RunWith(Parameterized.class)
 public class RequestValidatorTest {
-
-    /**
-     * WebhookSignatureTestCase
-     */
-    public static class WebhookSignatureTestCase {
-        public String name;
-        public String method;
-        public String secret;
-        public String url;
-        public String payload;
-        public String timestamp;
-        public String token;
-        public Boolean valid;
-        public String reason;
-    }
-
     /**
      * Error Map that maps test data expected outcome to actual error message.
      */
@@ -64,11 +38,10 @@ public class RequestValidatorTest {
                     "The Claim 'payload_hash' is set but actual payload is missing.");
             put("invalid jwt: claim payload_hash is not set but payload is present",
                     "The Claim 'payload_hash' is not set but payload is present.");
-            put("invalid jwt: signing method none is invalid", "The signing method is invalid.");
+            put("invalid jwt: signing method none is invalid", "The signing method 'none' is invalid.");
 
         }
     };
-
     private final WebhookSignatureTestCase testCase;
 
     public RequestValidatorTest(String testName, WebhookSignatureTestCase testCase) {
@@ -76,27 +49,27 @@ public class RequestValidatorTest {
     }
 
     @Parameters(name = "{0}")
-    public static Collection<Object[]> data() throws JsonParseException, JsonMappingException, IOException {
+    public static Collection<Object[]> data() throws IOException {
         List<WebhookSignatureTestCase> testCases = new ObjectMapper().readValue(
-                Resources.class.getResourceAsStream("/webhook_test_data.json"),
+                Resources.readResourceText("/webhook_test_data.json"),
                 new TypeReference<List<WebhookSignatureTestCase>>() {
                 });
 
         return testCases.stream()
-                .map(tc -> new Object[]{ tc.name, tc })
+                .map(tc -> new Object[]{tc.name, tc})
                 .collect(Collectors.toList());
     }
 
     @Test
     public void testWebhookSignature() throws Throwable {
-        RequestValidator validator = new RequestValidator(testCase.secret);
+        RequestValidator validator = new RequestValidator(testCase.secret != null ? testCase.secret : "");
 
         Clock clock = mock(Clock.class);
         Date clockDate = spy(Date.from(OffsetDateTime.parse(testCase.timestamp).toInstant()));
         when(clock.getToday()).thenReturn(clockDate);
 
         ThrowingRunnable runnable = () -> validator.validateSignature(clock, testCase.token, testCase.url,
-                (testCase.payload == null) ? null : testCase.payload.getBytes(Charset.forName("UTF-8")));
+                (testCase.payload == null) ? null : testCase.payload.getBytes(StandardCharsets.UTF_8));
 
         if (testCase.valid) {
             runnable.run();
@@ -111,5 +84,20 @@ public class RequestValidatorTest {
         RequestValidationException err = assertThrows(RequestValidationException.class, runnable);
         assertTrue(String.format("Expected error message containing: %s (originally %s) but was: %s", expectedError,
                 testCase.reason, err.getMessage()), err.getMessage().contains(expectedError));
+    }
+
+    /**
+     * WebhookSignatureTestCase
+     */
+    public static class WebhookSignatureTestCase {
+        public String name;
+        public String method;
+        public String secret;
+        public String url;
+        public String payload;
+        public String timestamp;
+        public String token;
+        public Boolean valid;
+        public String reason;
     }
 }
